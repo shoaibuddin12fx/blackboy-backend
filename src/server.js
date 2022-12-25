4b389f4bbfe91610415ab1f690ab6a59c95641df
anas ahmed <anas.ahmed142@gmail.com>
2022-12-25 22:52:52 +0500

    a

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const PORT = 4000;
 const MONGO_USER = "admin";
 const MONGO_PASSWORD = "hotmail1VU";
 const MONGO_DB = "bbh";
-const MONGO_URL = "127.0.0.1:27017";
+const MONGO_URL = "24.199.72.226:27017";
 const sendGridKey = "";
 // const dbString = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_URL}/${MONGO_DB}?retryWrites=true&w=majority`;
 // mongodb://admin:hotmail1VU@127.0.0.1:27017
